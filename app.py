36b221c574aa274525d51d1e2773306d1b9fad31
malinocom <127872796+malinocom@users.noreply.github.com>
2025-03-05 22:44:01 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-# Import the os module
-import os
-
-path = '/var/www'
-
-try:
-    os.chdir(path)
-    print("Current working directory: {0}".format(os.getcwd()))
-except FileNotFoundError:
-    print("Directory: {0} does not exist".format(path))
-except NotADirectoryError:
-    print("{0} is not a directory".format(path))
-except PermissionError:
-    print("You do not have permissions to change to {0}".format(path))
+print("Amirr")
